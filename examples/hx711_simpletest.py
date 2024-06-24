19678115d9eef5e1271add410ea0c8837c18e20d
Liz <liz@adafruit.com>
2024-06-24 16:20:29 -0400

    update pins for Feather

--- examples/hx711_simpletest.py
+++ examples/hx711_simpletest.py
@@ -8,9 +8,9 @@ import digitalio
 from adafruit_hx711.hx711 import HX711
 from adafruit_hx711.analog_in import AnalogIn
 
-data = digitalio.DigitalInOut(board.D2)
+data = digitalio.DigitalInOut(board.D5)
 data.direction = digitalio.Direction.INPUT
-clock = digitalio.DigitalInOut(board.D3)
+clock = digitalio.DigitalInOut(board.D6)
 clock.direction = digitalio.Direction.OUTPUT
 
 hx711 = HX711(data, clock)
